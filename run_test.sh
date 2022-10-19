e62434198a81e1976f09bac7ef92fb501e14ccec
yangxue <yangxue0827@126.com>
2022-10-19 09:35:19 +0800

    update h2rbox 3x model and log

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,2,1,3 PORT=29817 \
-./tools/dist_test.sh configs/h2rbox/h2rbox_r50_adamw_fpn_1x_dota_ms_le90.py \
-        work_dirs/h2rbox_r50_adamw_fpn_1x_dota_ms_le90/latest.pth 4 --format-only\
-        --eval-options submission_dir=work_dirs/h2rbox_r50_adamw_fpn_1x_dota_ms_le90/Task1_results
+CUDA_VISIBLE_DEVICES=0,1,2,3 PORT=29817 \
+./tools/dist_test.sh configs/h2rbox/h2rbox_r50_adamw_fpn_3x_dota_le90.py \
+        work_dirs/h2rbox_r50_adamw_fpn_3x_dota_le90/latest.pth 4 --format-only\
+        --eval-options submission_dir=work_dirs/h2rbox_r50_adamw_fpn_3x_dota_le90/Task1_results
