6c58dd0fe371cb5496715b94699f065c990b4463
yangxue <yangxue0827@126.com>
2023-02-10 16:02:18 +0800

    Update rotated_retinanet_hbb_r50_fpn_1x_dior_oc.py

--- configs/rotated_retinanet/rotated_retinanet_hbb_r50_fpn_1x_dior_oc.py
+++ configs/rotated_retinanet/rotated_retinanet_hbb_r50_fpn_1x_dior_oc.py
@@ -7,7 +7,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RResize', img_scale=(1024, 1024)),
+    dict(type='RResize', img_scale=(800, 800)),
     dict(type='RRandomFlip', flip_ratio=0.5, version=angle_version),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
